9cbd1e702d86fddc0caa4041fb0dc1237e9b93b7
Lynn Wolters <lynn.wolters27@gmail.com>
2024-09-30 00:50:33 +0200

    New

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,9 +1,13 @@
-document.querySelectorAll(".accordion-button").forEach(acc => {
-    acc.addEventListener("click", () => {
-        document.querySelectorAll(".accordion-panel").forEach(panel => {
-            panel.classList.remove("accordion-panel-toggle");
-        });
-        const accordionPanel = acc.nextElementSibling;
-        accordionPanel.classList.toggle("accordion-panel-toggle");
+document.querySelectorAll("main section:nth-of-type(5) ul li button").forEach(accordion => {
+    accordion.addEventListener("click", () => {
+        const accordionPanel = accordion.nextElementSibling;
+        if (accordionPanel.classList.contains("accordion-toggle")) {
+            accordionPanel.classList.remove("accordion-toggle");
+        } else {
+            document.querySelectorAll("main section:nth-of-type(5) ul li div").forEach(panel => {
+                panel.classList.remove("accordion-toggle");
+            });
+            accordionPanel.classList.add("accordion-toggle");
+        }
     });
 });
